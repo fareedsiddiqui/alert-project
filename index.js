219bcbbed1827842ae1e70173104b93270a003b9
fareedsiddiqui <fareedsidd3@gmail.com>
2020-03-06 16:35:26 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+alert (" percentag calculator")
+
+var percent = prompt ("enter your percantage %" , "")
+
+
+
+// var secondVal = prompt ("enter your 2nd number" , "")
+
+if (percent >= 90 &&  percent < 101 ){
+alert("your garde is" + " " + "A+" )
+alert  ("CONGRATULATIONS YOU CAN PROCEED NOW")
+  var firstName= prompt ("enter your first name", "")
+
+                    var lastName= prompt ("enter your last name", "")
+
+                    alert (firstName + "  "+ lastName  + " your % is more than 70% that's why you are selected" )
+}
+
+else if (percent >= 80 &&  percent < 90 ){
+    alert("your garde is" + " " + "A" )
+    alert  ("CONGRATULATIONS YOU CAN PROCEED NOW")
+  var firstName= prompt ("enter your first name", "")
+
+                    var lastName= prompt ("enter your last name", "")
+
+                    alert (firstName + "  "+ lastName + " your % is more than 70% that's why you are selected" )
+   
+    }
+
+    else if (percent >= 70 &&  percent < 80 ){
+        alert("your garde is" + " " + "B" )
+        alert  ("CONGRATULATIONS YOU CAN PROCEED NOW")
+  var firstName= prompt ("enter your first name", "")
+
+                    var lastName= prompt ("enter your last name", "")
+
+                    alert (firstName + "  "+ lastName + " your % is more than 70% that's why you are selected" )
+        }
+        else if (percent >= 60 &&  percent < 70 ){
+            alert("your garde is" + " " + "C+" )
+            }
+
+            else if (percent >= 50 &&  percent < 60 ){
+                alert("your garde is" + " " + "C" )
+                }
+                else if( percent >100){
+                    alert(" insaan ka bacha ban k sahi percentage likh")
+                }
+                
+
+
+                    // else if (B ){
+                    //     alert ("congratulations you can proceed now")
+                    // }
+                    else  {
+                        alert("sorry you are failed" )
+                        }
+                    
+                    
