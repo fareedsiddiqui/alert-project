0f3be53cbcb4f733c62085c92f73ec829497b78e
fareedsiddiqui <61496577+fareedsiddiqui@users.noreply.github.com>
2020-03-09 15:59:44 +0500

    array

--- index.js
+++ index.js
@@ -1,59 +1,18 @@
-alert (" percentag calculator")
+var students = ["ali","fareed" , "sidd", "najaf" , "ali","fareed" , "sidd", "najaf"]
+console.log(students)
+students.pop()
 
-var percent = prompt ("enter your percantage %" , "")
+console.log(students)
 
+students.push("muhammad", "ibrahim")
 
+console.log(students)
 
-// var secondVal = prompt ("enter your 2nd number" , "")
+students.shift()
+console.log(students)
 
-if (percent >= 90 &&  percent < 101 ){
-alert("your garde is" + " " + "A+" )
-alert  ("CONGRATULATIONS YOU CAN PROCEED NOW")
-  var firstName= prompt ("enter your first name", "")
+students.unshift( "fareed")
+console.log(students)
 
-                    var lastName= prompt ("enter your last name", "")
-
-                    alert (firstName + "  "+ lastName  + " your % is more than 70% that's why you are selected" )
-}
-
-else if (percent >= 80 &&  percent < 90 ){
-    alert("your garde is" + " " + "A" )
-    alert  ("CONGRATULATIONS YOU CAN PROCEED NOW")
-  var firstName= prompt ("enter your first name", "")
-
-                    var lastName= prompt ("enter your last name", "")
-
-                    alert (firstName + "  "+ lastName + " your % is more than 70% that's why you are selected" )
-   
-    }
-
-    else if (percent >= 70 &&  percent < 80 ){
-        alert("your garde is" + " " + "B" )
-        alert  ("CONGRATULATIONS YOU CAN PROCEED NOW")
-  var firstName= prompt ("enter your first name", "")
-
-                    var lastName= prompt ("enter your last name", "")
-
-                    alert (firstName + "  "+ lastName + " your % is more than 70% that's why you are selected" )
-        }
-        else if (percent >= 60 &&  percent < 70 ){
-            alert("your garde is" + " " + "C+" )
-            }
-
-            else if (percent >= 50 &&  percent < 60 ){
-                alert("your garde is" + " " + "C" )
-                }
-                else if( percent >100){
-                    alert(" insaan ka bacha ban k sahi percentage likh")
-                }
-                
-
-
-                    // else if (B ){
-                    //     alert ("congratulations you can proceed now")
-                    // }
-                    else  {
-                        alert("sorry you are failed" )
-                        }
-                    
-                    
+students.splice(5,2, "najjo")
+console.log(students)
